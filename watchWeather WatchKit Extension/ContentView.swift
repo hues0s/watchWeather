5c2ac58999d7dc1d0387b980bf4a36e036dde3d0
hues0s <hector.ullate@gmail.com>
2022-06-11 10:19:27 +0200

    Initial Commit

---
+++ watchWeather WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  watchWeather WatchKit Extension
+//
+//  Created by Héctor Ullate on 11/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ watchWeather WatchKit Extension/watchWeatherApp.swift
@@ -0,0 +1,19 @@
+//
+//  watchWeatherApp.swift
+//  watchWeather WatchKit Extension
+//
+//  Created by Héctor Ullate on 11/6/22.
+//
+
+import SwiftUI
+
+@main
+struct watchWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
